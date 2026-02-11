aaa4a8b196ae3d50e356e97bf19408de6ef9ee4b
sarimienak0016 <sarimienak0016@gmail.com>
2026-02-11 15:35:36 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -3,36 +3,36 @@ export async function onRequest(context) {
   const url = new URL(request.url);
   const path = url.pathname;
 
-  // ===== DAFTAR LINK AFFILIATE (BISA BANYAK) =====
   const affiliateLinks = [
     "https://shopee.co.id/affiliate_link_1",
     "https://shopee.co.id/affiliate_link_2",
     "https://shopee.co.id/affiliate_link_3"
   ];
 
-  // random affiliate
   const randomAffiliate =
     affiliateLinks[Math.floor(Math.random() * affiliateLinks.length)];
 
-  // tujuan akhir auto patch
   const finalTarget = `https://videy.co${path}`;
 
   const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="UTF-8">
-      <script>
-        window.location.href = "${randomAffiliate}";
-        setTimeout(function() {
-          window.location.href = "${finalTarget}";
-        }, 1500);
-      </script>
-    </head>
-    <body>
-      Redirecting...
-    </body>
-    </html>
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <script>
+      // coba buka shopee app
+      window.location.href = "${randomAffiliate}";
+
+      // kalau dalam 3 detik masih di browser → lanjut ke videy
+      setTimeout(function() {
+        window.location.href = "${finalTarget}";
+      }, 3000);
+    </script>
+  </head>
+  <body>
+    <p>Loading...</p>
+  </body>
+  </html>
   `;
 
   return new Response(html, {
